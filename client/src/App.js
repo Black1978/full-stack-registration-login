e57786cd6d3a661d5fb57e629ae5f6773cd4db83
Black1978 <chernyi.andrei@gmail.com>
2023-08-20 20:04:23 +0200

    The client side is added

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Register from './Register'
+
+function App(props) {
+    return (
+        <main>
+            <Register />
+        </main>
+    )
+}
+
+export default App
